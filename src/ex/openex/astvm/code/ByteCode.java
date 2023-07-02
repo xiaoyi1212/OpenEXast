c4655476190239d2ccd033c046612eb01a1ccfc9
XIAOYI12 <100394666+xiaoyi1212@users.noreply.github.com>
2023-07-03 01:12:26 +0800

    Delete src/ex/openex directory

--- src/ex/openex/astvm/code/ByteCode.java
+++
@@ -1,8 +0,0 @@
-package ex.openex.astvm.code;
-
-import ex.openex.astvm.exe.Executor;
-import ex.openex.exception.VMRuntimeException;
-
-public interface ByteCode {
-    public void executor(Executor executor)throws VMRuntimeException;
-}
--- src/ex/openex/astvm/code/struct/StructCode.java
+++
@@ -1,6 +0,0 @@
-package ex.openex.astvm.code.struct;
-
-import ex.openex.astvm.code.ByteCode;
-
-public abstract class StructCode implements ByteCode {
-}
